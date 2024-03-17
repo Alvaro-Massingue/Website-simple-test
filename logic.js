96725c82301a99df34565fafc9df34b3cdd19552
Alvaro Massingue <alvaroruimassingue658@gmail.com>
2024-03-17 03:57:19 +0000

    Dando os primeiros passos da construição do site

---
+++ logic.js
@@ -0,0 +1,3 @@
+confirm("Saudações, esse ainda não é um website funcional, é só a estrutura do que ainda esta por vir. espero pelo seu feedback");
+document.getElementById("aaa").innerHTML="IT TO THE WORLD";
+document.getElementById("bbb").innerHTML="Mantenha-se informado com a IT TO THE WORLD";
